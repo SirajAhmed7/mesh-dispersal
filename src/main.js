f03e1809d6058d477ad25e8fd0067f92a79af9d2
SirajAhmed <sirajahmedkhatri32@gmail.com>
2026-02-09 02:01:18 +0530

    Make layout and animation responsive

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import "lenis/dist/lenis.css";
 import "./style.css";
 
+import { inject } from "@vercel/analytics";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Lenis from "lenis";
 import * as THREE from "three";
-import { inject } from "@vercel/analytics";
 
 import DispersalObject from "./DispersalObject";
 
@@ -44,7 +44,6 @@ const sphereGeometry = new THREE.IcosahedronGeometry(2, 20);
 
 const sphere = new DispersalObject(sphereGeometry);
 scene.add(sphere.mesh);
-sphere.mesh.position.x = -3;
 
 // DONUT GEOMETRY
 // const boxGeometry = new THREE.BoxGeometry(2.75, 2.75, 2.75, 20, 20, 20);
@@ -55,7 +54,7 @@ donutGeometry.rotateZ(Math.PI * 0.125);
 // sphereGeometry.rotateY(Math.PI * 0.25);
 const donut = new DispersalObject(donutGeometry);
 scene.add(donut.mesh);
-donut.mesh.position.x = -15;
+
 // box.mesh.rotation.y = Math.PI * 0.385;
 
 // gui.add(box.mesh.position, "x").min(-100).max(-3).step(1).name("BoxX").listen();
@@ -67,7 +66,38 @@ capsuleGeometry.rotateY(Math.PI * -0.125);
 capsuleGeometry.rotateZ(Math.PI * -0.125);
 const capsule = new DispersalObject(capsuleGeometry);
 scene.add(capsule.mesh);
-capsule.mesh.position.x = -15;
+
+const isMd = window.matchMedia("(max-width: 768px)");
+
+const xStart = { lg: -15, md: -8 };
+const xEnd = { lg: -3, md: -0 };
+const yPos = { lg: 0, md: -1.55 };
+
+if (!isMd.matches) {
+  sphere.mesh.position.x = -3;
+  donut.mesh.position.x = xStart.lg;
+  capsule.mesh.position.x = xStart.lg;
+
+  sphere.mesh.position.y = yPos.lg;
+  donut.mesh.position.y = yPos.lg;
+  capsule.mesh.position.y = yPos.lg;
+
+  sphere.mesh.scale.setScalar(1);
+  donut.mesh.scale.setScalar(1);
+  capsule.mesh.scale.setScalar(1);
+} else {
+  sphere.mesh.position.x = 0;
+  donut.mesh.position.x = xStart.md;
+  capsule.mesh.position.x = xStart.md;
+
+  sphere.mesh.position.y = yPos.md;
+  donut.mesh.position.y = yPos.md;
+  capsule.mesh.position.y = yPos.md;
+
+  sphere.mesh.scale.setScalar(0.7);
+  donut.mesh.scale.setScalar(0.7);
+  capsule.mesh.scale.setScalar(0.7);
+}
 
 /**
  * Sizes
@@ -89,6 +119,33 @@ window.addEventListener("resize", () => {
   // Update renderer
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+  // Object positions
+  if (!isMd.matches) {
+    sphere.mesh.position.x = -3;
+    donut.mesh.position.x = xStart.lg;
+    capsule.mesh.position.x = xStart.lg;
+
+    sphere.mesh.position.y = yPos.lg;
+    donut.mesh.position.y = yPos.lg;
+    capsule.mesh.position.y = yPos.lg;
+
+    sphere.mesh.scale.setScalar(1);
+    donut.mesh.scale.setScalar(1);
+    capsule.mesh.scale.setScalar(1);
+  } else {
+    sphere.mesh.position.x = 0;
+    donut.mesh.position.x = xStart.md;
+    capsule.mesh.position.x = xStart.md;
+
+    sphere.mesh.position.y = yPos.md;
+    donut.mesh.position.y = yPos.md;
+    capsule.mesh.position.y = yPos.md;
+
+    sphere.mesh.scale.setScalar(0.7);
+    donut.mesh.scale.setScalar(0.7);
+    capsule.mesh.scale.setScalar(0.7);
+  }
 });
 
 /**
@@ -103,10 +160,6 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.set(0, 0, 5);
 scene.add(camera);
-
-// // Controls
-// const controls = new OrbitControls(camera, canvas);
-// controls.enableDamping = true;
 
 /**
  * Renderer
@@ -123,104 +176,107 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  * Animate
  */
 
-// ScrollTrigger.create();
-// SPHERE
-gsap.fromTo(
-  sphere.material.uniforms.uProgress,
-  {
-    value: 0,
-  },
-  {
-    scrollTrigger: {
-      trigger: ".section-1",
-      start: "top top",
-      end: "+=100%",
-      scrub: true,
-    },
-    value: 1,
-  },
-);
+const mm = gsap.matchMedia();
 
-// DONUT
-gsap.fromTo(
-  donut.mesh.position,
+mm.add(
   {
-    x: -15,
+    isLg: "(min-width: 769px)",
+    isMd: "(max-width: 768px)",
   },
-  {
-    scrollTrigger: {
-      trigger: ".section-2",
-      start: "top 50%",
-      end: "top 15%",
-      scrub: true,
-    },
-    x: -3,
-  },
-);
-gsap.fromTo(
-  donut.material.uniforms.uProgress,
-  {
-    value: 0,
-  },
-  {
-    scrollTrigger: {
-      trigger: ".section-2",
-      start: "top top",
-      end: "+=100%",
-      scrub: true,
-    },
-    value: 1,
-  },
-);
+  (context) => {
+    const { isLg, isMd } = context.conditions;
+    const tweenXStart = isLg ? xStart.lg : xStart.md;
+    const tweenXEnd = isLg ? xEnd.lg : xEnd.md;
 
-// CAPSULE
-gsap.fromTo(
-  capsule.mesh.position,
-  {
-    x: -15,
-  },
-  {
-    scrollTrigger: {
-      trigger: ".section-3",
-      start: "top 50%",
-      end: "top 15%",
-      scrub: true,
-    },
-    x: -3,
-  },
-);
-gsap.fromTo(
-  capsule.material.uniforms.uProgress,
-  {
-    value: 0,
-  },
-  {
-    scrollTrigger: {
-      trigger: ".section-3",
-      start: "top top",
-      end: "+=100%",
-      scrub: true,
-    },
-    value: 1,
+    // SPHERE
+    gsap.fromTo(
+      sphere.material.uniforms.uProgress,
+      {
+        value: 0,
+      },
+      {
+        scrollTrigger: {
+          trigger: ".section-1",
+          start: "top top",
+          end: "+=100%",
+          scrub: true,
+        },
+        value: 1,
+      },
+    );
+
+    // DONUT
+    gsap.fromTo(
+      donut.mesh.position,
+      {
+        x: tweenXStart,
+      },
+      {
+        scrollTrigger: {
+          trigger: ".section-2",
+          start: "top 50%",
+          end: "top 15%",
+          scrub: true,
+        },
+        x: tweenXEnd,
+      },
+    );
+    gsap.fromTo(
+      donut.material.uniforms.uProgress,
+      {
+        value: 0,
+      },
+      {
+        scrollTrigger: {
+          trigger: ".section-2",
+          start: "top top",
+          end: "+=100%",
+          scrub: true,
+        },
+        value: 1,
+      },
+    );
+
+    // CAPSULE
+    gsap.fromTo(
+      capsule.mesh.position,
+      {
+        x: tweenXStart,
+      },
+      {
+        scrollTrigger: {
+          trigger: ".section-3",
+          start: "top 50%",
+          end: "top 15%",
+          scrub: true,
+        },
+        x: tweenXEnd,
+      },
+    );
+    gsap.fromTo(
+      capsule.material.uniforms.uProgress,
+      {
+        value: 0,
+      },
+      {
+        scrollTrigger: {
+          trigger: ".section-3",
+          start: "top top",
+          end: "+=100%",
+          scrub: true,
+        },
+        value: 1,
+      },
+    );
   },
 );
 
 const tick = () => {
-  // // Timer
-  // timer.update();
-  // const elapsedTime = timer.getElapsed();
-
-  // Update uniforms
-  // material.uniforms.uTime.value = elapsedTime;
-
-  // // Update controls
-  // controls.update();
-
   // Render
   renderer.render(scene, camera);
 
   // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
+  requestAnimationFrame(tick);
 };
 
 tick();
